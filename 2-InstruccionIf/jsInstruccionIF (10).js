957eac22083d19e33b08eb1d0258f988e6222eb9
AlanCintora <alancintora99@gmail.com>
2020-02-11 13:16:24 -0300

    termine los ejercicios de IF

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,19 @@
 function mostrar()
-{
-	//Genero el número RANDOM entre 1 y 10 
+{var num1;
+	num1 = Math.floor(Math.random()*(11-1))+1;
+		
+		if(num1 >= 9)
+		{
+			alert("EXCELENTE");
+		}
+		else if(num1 >= 4 && num1 < 9)
+		{
+			alert("APROBÓ");
+		}
+			else
+			{
+				alert("Vamos, la proxima se puede");
+			}
 	
 
 }//FIN DE LA FUNCIÓN
