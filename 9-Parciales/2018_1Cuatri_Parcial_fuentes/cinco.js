bf06dc4e4c273999211e652d64a88f3ee7b39cbb
AlanCintora <alancintora99@gmail.com>
2020-02-17 01:03:00 -0300

    realice algunos ejercicios de parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,23 @@
 function mostrar()
-{
+{var planeta;
+ planeta = prompt("Ingrese un planeta: ");
+    switch(planeta){
+        case "venus":
+        case "mercurio":
+            alert("acá hace mas calor");
+            break;
+        case "tierra":
+            alert("acá es donde vivimos");
+            break;
+        case "marte":
+        case "jupiter":
+        case "saturno":
+        case "neptuno":
+        case "urano":
+            alert("acá hace mas frio");
+            break;
+        default:
+            alert("Este no es un planeta ");
 
+    }
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,21 @@
 function mostrar()
-{
+{var num1;
+ var num2;
+ num1 = prompt("Ingrese un número:");
+ num2 = prompt("Ingrese un número:");
+   if(num1 == num2){
+       alert(num1 + num2);
+   }
+   num1 = parseInt(num1);
+   num2 = parseInt(num2);
 
+        if(num1 > num2){
+       alert(num1 - num2);
+         }
+         else if(num1 < num2){
+       alert(num1 + num2 );
+         }
+        if((num1 + num2) > 10){
+         alert("La suma es " + (num1+num2) + " y supero el 10");
+         }
 }
